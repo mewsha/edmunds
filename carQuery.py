c211397c31268ef587cde8257db9c6da0f62d8be
mewsha@users.noreply.github.com <users.noreply.github.com>
2015-01-08 18:07:45 -0800

    add package and engine searches

--- carQuery.py
+++ carQuery.py
@@ -8,9 +8,9 @@ class carQuery(object):
         self.handler = ed.edmunds()
 
 
-    def getYearInfo(self, make, model, year):
+    def getStyleInfo(self, make, model, year):
         try:
-            jsonData = self.handler.getModelYearInfo(make, model,year)
+            jsonData = self.handler.getModelYearJson(make, model,year)
             carInfo = self.handler.formatJSON(jsonData)
             self.handler.printModelInfo(carInfo)
         except Exception as e:
@@ -21,14 +21,43 @@ class carQuery(object):
     def getManufacturerInfo(self, maker):
         """Prints the models and years manufatured for a maker"""
         try:
-            jsonData = self.handler.getMakeInfo(maker)
+            jsonData = self.handler.getMakeJson(maker)
             makerInfo = self.handler.formatJSON(jsonData)
             self.handler.printMakeInfo(makerInfo)
         except Exception as e:
             print "An exception occured while getting manufacturer info"
-            print e
-		
+            print type(e), e
 
 
-		
+    def getPackageInfo(self, styleId):
+        try:
+            jsonData = self.handler.getPackageJson(styleId)
+            packageInfo = self.handler.formatJSON(jsonData)
+            self.handler.printPackageInfo(packageInfo)
+        except Exception as e:
+            print "An exception occured while getting package info"
+            print type(e), e 
+
+    def getEngineInfo(self, styleId):
+        try:
+            jsonData = self.handler.getPackageJson(styleId)
+            packageInfo = self.handler.formatJSON(jsonData)
+            self.handler.printEngineInfo(packageInfo)
+        except Exception as e:
+            print "An exception occured while getting engine info"
+            print type(e), e 
+        
+
+    def compareEngines(self, style1id, style2id):
+        """compares engines for two cars"""
+        try:
+            jsonData1 = self.handler.getPackageJson(style1id)
+            jsonData2 = self.handler.getPackageJson(style2id)
+            engine1data = self.handler.formatJSON(jsonData1)
+            engine2data = self.handler.formatJSON(jsonData2)
+            self.handler.printEnginesSideBySide(engine1data, engine2data)
+        except Exception as e:
+            print "An exception occured while comparing engines"
+            print type(e), e
+        
 
